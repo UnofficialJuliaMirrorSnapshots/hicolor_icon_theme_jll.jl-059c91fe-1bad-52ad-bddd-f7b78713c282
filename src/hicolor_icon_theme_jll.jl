2242b57477642229dd9a5575d1f6d1d1aa25db20
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-30 18:26:12 +0000

    Snapshot of branch master taken on 2019-10-30T14:26:11.22-04:00 from "https://github.com/UnofficialJuliaMirror/hicolor_icon_theme_jll.jl-059c91fe-1bad-52ad-bddd-f7b78713c282"

---
+++ src/hicolor_icon_theme_jll.jl
@@ -0,0 +1,31 @@
+module hicolor_icon_theme_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("059c91fe-1bad-52ad-bddd-f7b78713c282"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "hicolor_icon_theme", artifacts_toml) for e in artifacts["hicolor_icon_theme"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load hicolor_icon_theme; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module hicolor_icon_theme_jll
